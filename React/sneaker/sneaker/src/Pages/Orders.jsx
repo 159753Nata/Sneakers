57311181a473649e3e02752507ac54d47638697c
Your Name <you@example.com>
2023-05-30 20:46:09 +0300

    добавили подбор и удаление в поиске

---
+++ React/sneaker/sneaker/src/Pages/Orders.jsx
@@ -0,0 +1,20 @@
+import Header from '../Component/Header';
+import Drawer from '../Component/Drawer';
+import Card from '../Component/Card'
+
+function Orders({isVisible, setIsVisible, drawerItems, orderItems, setOrderItems}){
+    return(
+        <>
+        <Header isVisible={isVisible} setIsVisible={setIsVisible}/>
+        <Drawer drawerItems={drawerItems} isVisible={isVisible} setIsVisible={setIsVisible} />
+        <div className="content">
+        <div className="ds-f">
+           {orderItems.map((elem, index) => <Card drawerItems={drawerItems} setOrderItems={setOrderItems} 
+           title={elem.title} price={elem.price} imageUrl={elem.imageUrl} key={index}/>) }
+        </div>
+        </div>
+        </>
+    );
+}
+
+export default Orders;
